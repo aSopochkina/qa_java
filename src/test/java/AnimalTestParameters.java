bcfac11b098dc7069a46560cefdfe852aa218453
Anastasiya Sopochkina <rojokrojokrojok.9@yandex.ru>
2024-02-04 23:33:42 +0300

    feat_1

---
+++ src/test/java/AnimalTestParameters.java
@@ -0,0 +1,44 @@
+import com.example.Animal;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class AnimalTestParameters {
+    private Animal animal;
+    private static final String ANIMAL_KIND_HERBIVORE = "Травоядное";
+    private static final String ANIMAL_KIND_PREDATOR = "Хищник";
+    private static final List<String> FOODS_HERBIVORE = List.of("Трава", "Различные растения");
+    private static final List<String> FOODS_PREDATOR = List.of("Животные", "Птицы", "Рыба");
+
+    private String animalKind;
+    private List<String> foods;
+
+    public AnimalTestParameters(String animalKind, List<String> foods) {
+        this.animalKind = animalKind;
+        this.foods = foods;
+    }
+    @Parameterized.Parameters
+    public static Object[][] getParameters() {
+        return new Object[][]{
+                {ANIMAL_KIND_HERBIVORE, FOODS_HERBIVORE},
+                {ANIMAL_KIND_PREDATOR, FOODS_PREDATOR}
+        };
+    }
+
+    @Before
+    public void setUp() {
+        animal = new Animal();
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        List<String> actual = animal.getFood(animalKind);
+        assertEquals("Список еды для животных не совпадает с ожидаемым",
+                foods, actual);
+    }
+}
+
---
+++ src/test/java/CatTest.java
@@ -0,0 +1,36 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.assertEquals;
+
+    @RunWith(MockitoJUnitRunner.class)
+    public class CatTest {
+        private Cat cat;
+
+        @Mock
+        Feline feline;
+
+        @Before
+        public void setUp() {
+            cat = new Cat(feline);
+        }
+
+        @Test
+        public void testGetSound() {
+            String actual = cat.getSound();
+
+            assertEquals("Мяу", actual);
+        }
+
+        @Test
+        public void testGetFood() throws Exception {
+            cat.getFood();
+            Mockito.verify(feline).eatMeat();
+        }
+
+}
